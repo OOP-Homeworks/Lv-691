e9c11e04c28af4c68896db7e19abff9fc38f5ca7
Margo <97248807+Bahinska@users.noreply.github.com>
2022-04-29 01:13:32 +0300

    Bahinska_hw2

--- Program.cs
+++ Program.cs
@@ -1,78 +1,80 @@
 ﻿using System;
 
-namespace hw1
+namespace hw2
 {
     class Program
     {
+            enum HTTPError
+            {
+               BadRequest=400,
+               Unauthorized,
+               PaymentRequired,
+               Forbidden,
+               NotFound,
+               MethodNotAllowed,
+              NotAcceptable,
+        }
+            struct Dog
+            {
+                public string name;
+                public string mark;
+                public int age;
+
+                public override string ToString()
+            {
+                return string.Format($"Name: {name}\nMark: {mark}\nAge: {age}");
+            }
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Task 1\n1)");
-            int a, b;
-            Console.Write("a = ");
-            a = System.Convert.ToInt32(Console.ReadLine());
-            Console.Write("b = ");
-            b = System.Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Sum {a+b}");
-            Console.WriteLine($"Dif {a - b}");
-            Console.WriteLine($"Mul {a * b}");
-            Console.WriteLine($"Div {a / b}");
-
-            Console.WriteLine("\n2)");
-            Console.WriteLine("How are you?");
-            string answer = Console.ReadLine();
-            Console.Write($"You are {answer}\n");
-
-            Console.WriteLine("\n3)");
-            Console.WriteLine("Input char:");
-            char x,y,z;
-            x= System.Convert.ToChar(Console.ReadLine());
-            y = System.Convert.ToChar(Console.ReadLine());
-            z = System.Convert.ToChar(Console.ReadLine());
-            Console.WriteLine($"You enter: {x}, {y}, {z}");
-
-            Console.WriteLine("\n4)");
-
-            static bool check(int a, int b)
+            Console.WriteLine("1)");
+            bool check(float num)
             {
-                bool result=false;
-                if (a > 0 && b > 0)
-                {
-                    result = true;
-                }
-                return result;
+                return (num >= -5 && num <= 5);
             }
-            int s, m;
-            Console.Write("first integer = ");
-            s = System.Convert.ToInt32(Console.ReadLine());
-            Console.Write("second integer = ");
-            m = System.Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(check(s, m) ? "they are both positive" : "one or two are not positive\n");
+            Console.WriteLine("enter three float num: ");
+            float num1 = Single.Parse(Console.ReadLine());
+            float num2 = Single.Parse(Console.ReadLine());
+            float num3 = Single.Parse(Console.ReadLine());
+            Console.WriteLine(check(num1) ? "num1 is in the range " : "num1 is not in the range");
+            Console.WriteLine(check(num2) ? "num2 is in the range " : "num2 is not in the range");
+            Console.WriteLine(check(num3) ? "num3 is in the range " : "num3 is not in the range");
 
-            Console.WriteLine("Homework 1\n1)");
-            int side;
-            Console.Write("enter side of square  ");
-            side = System.Convert.ToInt32(Console.ReadLine());
-            int area = side * side;
-            int per = 4 * side;
-            Console.WriteLine($"Area: {area}  Perimeter: {per}");
+            Console.WriteLine("2)");
+            Console.WriteLine("enter three integers: ");
+            int max(int num1, int num2, int num3)
+            {
+                if (num1 >= num2 && num1 >= num3) return num1;
+                else if (num2 >= num1 && num2 >= num3) return num2;
+                else if (num3 >= num1 && num3 >= num2) return num3;
+                return 0; //without this Error  not all code paths return a value 
 
-            Console.WriteLine("\n2)");
-            string name;
-            Console.WriteLine("What is your name?");
-            name = Console.ReadLine();
-            Console.WriteLine($"How old are you, {name}?");
-            int age = System.Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Your name is {name} and you are {age} years old");
+            }
+            int min(int num1, int num2, int num3)
+            {
+                if (num1 <= num2 && num1 <= num3) return num1;
+                else if (num2 <= num1 && num2 <= num3) return num2;
+                else if (num3 <= num1 && num3 <= num2) return num3;
+                return 0; //without this Error  not all code paths return a value 
+            }
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            int third = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Max: { max(first, second, third)}");
+            Console.WriteLine($"Min: { min(first, second, third)}");
 
-            Console.WriteLine("\n3)");
-            double radius;
-            Console.Write("enter the radius of circle  ");
-            radius = System.Convert.ToInt32(Console.ReadLine());
-            double L = 2*3.14*radius;
-            double S = 3.14*radius*radius;
-            double V = 4 / 3 * 3.14 * radius * radius * radius;
-            Console.WriteLine($"Area: {S}  Length: {L}  Volume: {V}");
+            Console.WriteLine("3)");
+            Console.WriteLine("Enter num of HTTP error (400, ..., 406");
+            int errornum = Convert.ToInt32(Console.ReadLine());
+            HTTPError error =(HTTPError) errornum;
+            Console.WriteLine( error);
 
+            Console.WriteLine("4)");
+            Dog myDog;
+            myDog.name = "Javelin";
+            myDog.mark = "Amerikana";
+            myDog.age = 8;
+            Console.WriteLine(myDog);
         }
     }
 }
