797c61b5b87df9ef4a388c57b350f0c543f8893d
OksanaM <oks.sova2@gmail.com>
2022-04-30 02:11:44 +0300

    Add Solution HW1

---
+++ Mala_HW1(TstPR)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mala_HW1_TstPR_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
