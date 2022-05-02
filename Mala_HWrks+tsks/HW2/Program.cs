7fde3cd4551c836b351ffadc65d5011875690a48
OksanaM <oks.sova2@gmail.com>
2022-05-02 03:10:03 +0300

    AddHW1.1

---
+++ Mala_HWrks+tsks/HW2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter three three decimal numbers one by one" + 
+                "After entering each number, press 'Enter'");
+            float firstNum = float.Parse(Console.ReadLine());
+            float secondNum = float.Parse(Console.ReadLine());
+            float thirdNum = float.Parse(Console.ReadLine());
+
+            NumInRange checkNum = new();
+            checkNum.ChekNumIsInRange(firstNum, secondNum, thirdNum);
+        }
+    }
+}
