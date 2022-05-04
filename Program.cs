93a4004b04311dc9d9c7f7d1bb8eaed02799f1cb
Svitlana <103998860+Chynchenko@users.noreply.github.com>
2022-05-04 18:59:00 +0300

    Add files via upload
    Homework_3

--- Program.cs
+++ Program.cs
@@ -1,161 +1,208 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Homework_2
+namespace Homework_3
+
 {
     class Program
     {
-        enum TestCaseStatus { Pass, Fail, Blocked, WP, Unexecuted };
-
-        enum HTTPError
+        private static int Main(string[] args)
         {
-            BadRequest = 400,
-            Unauthorized,
-            PaymentRequired,
-            Forbidden,
-            NotFound
-        };
+            //1
 
-        struct RGB
-        {
-            public byte red;
-            public byte green;
-            public byte blue;
+            Console.WriteLine("Enter two int numbers");
 
-            public void ColorChoose ()
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            for (int i = a + 1; i < b; i++)
             {
-                Console.WriteLine("Please write three numbers of RGB (1 or 0)");
-                red = Convert.ToByte(Console.ReadLine());
-                green = Convert.ToByte(Console.ReadLine());
-                blue = Convert.ToByte(Console.ReadLine());
-
-                if (red == 0 && green == 0 && blue == 0)
-                {
-                    Console.WriteLine("The colour is white");
-                }
-                else if (red == 1 && green == 1 && blue == 1)
-                {
-                    Console.WriteLine("The colour is black");
-                }
-   
+                if (i % 3 == 0) count++;
             }
-            
-        }
+            Console.WriteLine($"Answer {count}\n");
 
-        struct Dog
-        {
-            public string name;
-            public string mark;
-            public int age;
+            //2
 
-            public void Info()
+            Console.WriteLine("Enter long word");
+            string str = Console.ReadLine();
+            char[] chars;
+            chars = str.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
             {
-                Console.WriteLine($"I have a dog {name}, mark {mark}, {age} years old");
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(chars[i]);
+                }
             }
-        }
 
-        static void Main(string[] args)
-        {
-            int a;
-            Console.WriteLine("Write the first number");
-            a = Convert.ToInt32(Console.ReadLine());
-            if (a > 0 & a <= 31)
-                Console.WriteLine("The day of month is correct");
+            //3
+
+            Console.WriteLine("Choose one of drinks: coffe/tea/juice/water");
+            string input = Console.ReadLine();
+            switch (input.ToLower())
+            {
+                case "coffe":
+                    Console.WriteLine("The price of coffe is 2.00");
+                    break;
+                case "tea":
+                    Console.WriteLine("The price of tea is 1.50");
+                    break;
+                case "juice":
+                    Console.WriteLine("The price of juice is 2.20");
+                    break;
+                case "water":
+                    Console.WriteLine("The price of water is 0,50");
+                    break;
+            }
+
+            //4
+            int list = 0;
+            List<int> lists = new List<int>();
+            do
+            {
+                Console.WriteLine("Write some integer");
+                list = Convert.ToInt32(Console.ReadLine());
+                if (list > 0)
+                {
+                    lists.Add(list);
+                }
+            }
+            while (list > 0);
+
+            if (lists.Count > 0)
+            {
+                double average = lists.AsQueryable().Sum() / (double)lists.Count();
+                Console.WriteLine($"Sum is {average}");
+            }
             else
-                Console.WriteLine("The day of month is incorrect");
-            int b;
-            Console.WriteLine("Write the second number");
-            b = Convert.ToInt32(Console.ReadLine());
-            if (b > 0 & b < 13)
-                Console.WriteLine("The number of month is correct");
-            else
-                Console.WriteLine("The number of month is incorrect");
+            {
+                Console.WriteLine("No positive integers");
+            }
 
 
-            double c;
-            Console.WriteLine("Write double number");
-            c = Convert.ToDouble(Console.ReadLine());
-            int t = (int)(c * 100);
-            double d = t % 10;
-            double f = (t % 100 - t % 10) / 10;
-            Console.WriteLine($"Sum is {d + f}");
+            //5
+            Console.WriteLine("Write a year");
+            int year;
+            year = Convert.ToInt32(Console.ReadLine());
+            static void IsLeapYear(int v, int year) => throw new NotImplementedException();
+            Console.WriteLine(true ? "The year is leap" : "The year is not leap");
 
 
-            int time;
-            Console.WriteLine("Write time");
-            time = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine((time > 6) && (time <= 9) ? "morning" :
-                (time > 9) && (time <= 15) ? "afternoon" :
-                (time > 15) && (time <= 23) ? "evening" :
-                (time >= 0) && (time <= 6) ? "night" :
-                "error");
+            //6
+            Console.WriteLine("Write a number");
+            int value = Convert.ToInt32(Console.ReadLine());
+            int res = 0;
+            while (value > 0)
+            {
+                res += value % 10;
+                value /= 10;
+                Console.WriteLine($"The sum is {res}");
+            }
+            return (res);
+
+            //7
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            Console.WriteLine("Write a number");
+            while (num < 0)
+            {
+                result += num % 10;
+                num /= 10;
+            }
+            if (result % 2 != 0)
+                Console.WriteLine("integer contains odd digits");
 
 
-            TestCaseStatus test1Status = TestCaseStatus.Pass;
-            if (test1Status == TestCaseStatus.Pass)
-                Console.WriteLine("test1Status is Pass");
-            else
-                Console.WriteLine("test1Status is not passed");
+            //1
+            Console.WriteLine("Enter some string");
+            string word = Console.ReadLine();
+            char[] letter;
+            letter = word.ToCharArray();
+            for (int l = 0; l < word.Length; l++)
+            {
+                if (word[l] == 'a')
+                    Console.WriteLine($"There are {letter[l]} letters 'a'");
+            }
+            for (int l = 0; l < word.Length; l++)
+            {
+                if (word[l] == 'o')
+                    Console.WriteLine($"There are {letter[l]} letters 'o'");
+            }
+            for (int l = 0; l < word.Length; l++)
+            {
+                if (word[l] == 'i')
+                    Console.WriteLine($"There are {letter[l]} letters 'i'");
+            }
+            for (int l = 0; l < word.Length; l++)
+            {
+                if (word[l] == 'e')
+                    Console.WriteLine($"There are {letter[l]} letters 'e'");
 
 
-            var struct1 = new RGB();
-            struct1.ColorChoose();
+                //2
+                Console.WriteLine("Write the nomber of month");
+                int month = Convert.ToInt32(Console.ReadLine());
+                switch (month)
+                {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        Console.WriteLine("There are 31 days in this month");
+                        break;
+                    case 2:
+                        Console.WriteLine("There are 28 or 29 days in this month");
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        Console.WriteLine("There are 30 days in this month");
+                        break;
+                }
 
+                //3
+                Console.WriteLine("Enter 10 integer numbers");
+                int[] nums = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    nums[i] = int.Parse(Console.ReadLine());
+                }
+                int sum1 = 0;
+                foreach (int i in nums)
+                {
+                    if (num >= 0 && i < 5)
+                    {
+                        sum1 += i;
+                    }
+                }
+                Console.WriteLine($"The sum or 5 first elements is {sum1}");
+                int sum2 = 0;
+                foreach (int i in nums)
+                {
+                    if (num < 0 && i < 5)
+                    {
+                        sum2 = nums[5] + nums[6] + nums[7] + nums[8] + nums[9];
+                    }
+                }
+                Console.WriteLine($"The sum of 5 last elements is {sum2}");
 
-            float one;
-            Console.WriteLine("Write the first float number");
-            one = Convert.ToSingle(Console.ReadLine());
-            float two;
-            Console.WriteLine("Write the second float number");
-            two = Convert.ToSingle(Console.ReadLine());
-            float three;
-            Console.WriteLine("Write the third number");
-            three = Convert.ToSingle(Console.ReadLine());
-            if ((one >= -5 && one <= 5) && (two >= -5 && two <= 5) && (three >= -5 && three <= 5))
-                Console.WriteLine("All numbers are in range -5.5");
-            else
-                Console.WriteLine("At least one number is not in range -5.5");
-
-
-            int first;
-            Console.WriteLine("Write the first number");
-            first = Convert.ToInt32(Console.ReadLine());
-            int second;
-            Console.WriteLine("Write the second number");
-            second = Convert.ToInt32(Console.ReadLine());
-            int third;
-            Console.WriteLine("Write the third number");
-            third = Convert.ToInt32(Console.ReadLine());
-            int e = Math.Max(first, second);
-            int l = Math.Max(e, third);
-            Console.WriteLine($"Max value is {l}");
-            int k = Math.Min(first, second);
-            int w = Math.Min(k, third);
-            Console.WriteLine($"Min value is {w}");
-
-
-            Console.WriteLine("Write the number of HTTP error from 400 to 404");
-            int errorNumber;
-            errorNumber = Convert.ToInt32(Console.ReadLine());
-            HTTPError error = (HTTPError)errorNumber;
-            Console.WriteLine(error);
-
-
-            Dog myDog;
-            myDog.name = "Lord";
-            myDog.mark = "Shpitz";
-            myDog.age = 2;
-            myDog.Info();
+                Console.ReadKey();
+            }
         }
     }
 }
 
 
 
-        
-        
 
 
 
 
 
-
+       
