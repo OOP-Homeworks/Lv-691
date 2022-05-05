a86719eb8a06d2bd001346140e2ef4ee5607aebc
Margo <97248807+Bahinska@users.noreply.github.com>
2022-05-05 23:57:33 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,92 +1,60 @@
 ﻿using System;
 
-namespace hw3
+namespace hw4
 {
+   /* In the method Main() create 6 objects of Person type and enter information about them.
+Then calculate and output on the console name and Age of each person; 
+Change the name of persons, which Age is less then 16, to "Very Young".
+Output information about all persons on the console.
+Find and output information about Persons with the same names (use ==).*/
+
     class Program
     {
+        public static void Output(Person[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                System.Console.WriteLine(arr[i].ToString());
+            }
+        }
         static void Main(string[] args)
         {
-            // ~~~~~~~~~~~~~~task 1~~~~~~~~~~~~~~~~~~~~
-            Console.WriteLine("Input string please: ");
-            string str =Console.ReadLine();
-            str = str.ToLower();
-            int count = 0;
-            for (int i = 0 ; i<str.Length; i++)
+            Person first = new Person();
+            Person second = new Person("Richard", new DateTime(2006,10,23));
+            Person third = new Person(second);
+            int size = 6;
+            Person[] array = new Person[size];
+            array[0] = first;
+            array[1] = second;
+            array[2] = third;
+
+            for(int i=3; i<size; i++)
             {
-                if (str[i] == 'a'|| str[i] == 'o'|| str[i] == 'i'|| str[i] == 'a') count++;
+                Person temp = new Person();
+                Console.WriteLine($"Enter {i+1} person's name and date of birth (dd/mm/yyyy)");
+                temp.Name = Console.ReadLine();
+                temp.BirthYear = Convert.ToDateTime(Console.ReadLine());
+                array[i] = temp;
             }
-            Console.WriteLine($"Answer:{count} \n");
-
-            // ~~~~~~~~~~~~~~task 2~~~~~~~~~~~~~~~~~~~~
-
-            bool right_input = false;
-            while(!right_input)
+            for (int i = 0; i < size; i++)
             {
-                Console.WriteLine("Enter number of your month");
-                int month = System.Convert.ToInt32(Console.ReadLine());
-
-                switch(month)
+                Console.WriteLine($"{array[i].Name} is {array[i].Age()} years old ");
+            }
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i].Age() < 16) array[i].Name = "Very Young";
+            }
+            Output(array, size);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i+1; j < size; j++) 
                 {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 8:
-                    case 10:
-                    case 12:
-                        Console.WriteLine("This month has 31 days");
-                        right_input = true;
-                        break;
-                    case 4:
-                    case 6:
-                    case 9:
-                    case 11:
-                        Console.WriteLine("This month has 30 days");
-                        right_input = true;
-                        break;
-                    case 2:
-                        Console.WriteLine("This month has 28 or even 29 days");
-                        right_input = true;
-                        break;
-                    default:
-                        Console.WriteLine("Wrong input. Let's try again ");
-                        break;
+                    if (array[i] == array[j])
+                    {
+                        Console.WriteLine($"Person {i+1} and persone {j+1} have the same name.");
+                    }
                 }
             }
-            // ~~~~~~~~~~~~~~task 3~~~~~~~~~~~~~~~~~~~~
-            Console.WriteLine("Enter 10 numbers: ");
-
-            int[] arr = new int[10];
-            int first_positive = 0;
-            for (int i=0; i<10; i++)
-            {
-                int n = System.Convert.ToInt32(Console.ReadLine());
-                arr[i] = n;
-                while(i<5)
-                {
-                   if(n>0) first_positive++;
-                    break;
-                }
-            }
-            if(first_positive==5)
-            {
-            int sum = 0;
-                for (int i = 0; i < 5; i++)
-                {
-                    sum += arr[i];
-                }
-                Console.WriteLine($"Answer: {sum}");
-            }
-            else
-            {
-            int prod = 1;
-                for (int i = 9; i >= 5; i--)
-                {
-                    prod *= arr[i];
-                }
-                Console.WriteLine($"Answer: {prod}");
-            }
-          
         }
     }
 }
