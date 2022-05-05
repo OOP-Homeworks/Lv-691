99de50aa3da6dfaadd23df825b4c8c527e96914f
OksanaM <oks.sova2@gmail.com>
2022-05-05 03:40:42 +0300

    Decision HW2

--- MalaOks_HW3_tsk3/MalaOks_HW3_tsk3/MalaOks_HW3_tsk3/CalculateCharactersInStr.cs
+++ MalaOks_HW3_tsk3/MalaOks_HW3_tsk3/MalaOks_HW3_tsk3/CalculateCharactersInStr.cs
@@ -8,5 +8,19 @@ namespace MalaOks_HW3_tsk3
 {
     internal class CalculateCharactersInStr
     {
+        public int ContCharacters(string sourse)
+        {
+            int count = 0;
+            string str = sourse.ToLower().Replace(" ", "");
+            foreach (var item in str)
+            {
+                if (item == 'a' || item == 'o' || item == 'i' || item == 'e')
+                {
+                    count++;
+                }
+
+            }
+            return count;   
+        }
     }
 }
--- MalaOks_HW3_tsk3/MalaOks_HW3_tsk3/MalaOks_HW3_tsk3/CheckPositiveSum5.cs
+++ MalaOks_HW3_tsk3/MalaOks_HW3_tsk3/MalaOks_HW3_tsk3/CheckPositiveSum5.cs
@@ -8,5 +8,36 @@ namespace MalaOks_HW3_tsk3
 {
     internal class CheckPositiveSum5
     {
+        public int Sum { get; set; }
+        //int sum = 0;
+        private void CalcSum (int start, int[] arr)
+        {
+            for (int i = start; i < arr.Length; i++)
+            {
+                Sum += arr[i];
+            }
+        }
+        public void SumOf5(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 0 & i <= 4)
+                {
+                    Sum += arr[i];
+                    //break;
+                }
+                else if (arr[i] < 0 || i <= 4)
+                {
+                    Sum = 0;
+                    for (int j = 5; j < arr.Length; j++)
+                    {
+                        Sum += arr[j];
+                    }
+                    break;
+                }
+            }
+            //Console.WriteLine($"Sum of 5 entered numbers = {Sum}");
+        }
+        
     }
 }
