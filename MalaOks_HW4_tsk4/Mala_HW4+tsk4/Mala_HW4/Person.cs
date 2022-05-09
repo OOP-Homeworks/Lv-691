cc3b7b9e7c47dd23c2e4ed6ecf7505e06ad442dc
OksanaM <oks.sova2@gmail.com>
2022-05-09 03:13:25 +0300

    Mala add hw4+tsk4

---
+++ MalaOks_HW4_tsk4/Mala_HW4+tsk4/Mala_HW4/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mala_HW4
+{
+    internal class Person
+    {
+        private DateTime birthYear;
+        private string name;
+        public DateTime BirthYear => birthYear;
+
+        public string Name => name;
+
+        public enum NameMonth
+        {
+            January = 1,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,   
+            December
+        }
+        public Person()
+        {
+            name = "Elen";
+            birthYear = new DateTime(2000, 03, 15);
+        }
+        public Person(string nm, DateTime dt)
+        {
+            name = nm;
+            birthYear = dt;
+        }
+
+        public int GetAge(DateTime date)
+        {
+            int age = (DateTime.Now.Date.Year - date.Year);
+            return age;
+        }
+
+        public static Person Input()
+        {
+            Console.WriteLine("Enter name of the person");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the date of birth of the person in format 'dd-mm-yyyy'");
+            DateTime.TryParse(Console.ReadLine(), out DateTime dt);
+            return new Person(name, dt);
+        }
+        public void ChangeName(string newName)
+        {
+            this.name = newName;
+        }
+        public override string ToString()
+        {
+            return $"Name = {Name}; " +
+                $"\n Date of birth = {birthYear.Day} {Enum.Parse(typeof(NameMonth),birthYear.Month.ToString())} {birthYear.Year}";
+        }
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+        public static bool operator ==(Person p1, Person p2)
+        {
+            return (p1.Name == p2.Name);
+        }
+        public static bool operator !=(Person p1, Person p2)
+        {
+            return !(p1.Name == p2.Name);
+        }
+
+    }
+}
---
+++ MalaOks_HW4_tsk4/Mala_HW4+tsk4/Mala_Tsk4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Mala_Tsk4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Output();
+
+            Car car1 = new Car("DBX", "white", 270000);
+            car1.Output();
+            var cars = new Car[4];
+            for (int i = 2; i < cars.Length; i++)
+            {
+                cars[0] = car;
+                cars[1] = car1;
+                cars[i] = Car.Input(i);
+            }
+            Console.WriteLine("Enter the percentage by which you want to reduce the cost");
+            double x = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i].ChangePrice(x);
+                cars[i].Output();
+            }
+            Console.WriteLine("Enter a color to repaint the car");
+            string newColor = Console.ReadLine();
+            string oldColor = "white";
+            for (int i = 0; i < cars.Length; i++)
+            {
+                if (cars[i].Color.Equals(oldColor))
+                {
+                    cars[i].Color = newColor;
+                    Console.WriteLine($"Car{i} color was changed to {newColor}");
+                }
+                else
+                {
+                    Console.WriteLine($"Car{i} color was not changed");
+                }
+            }
+            for (int i = 0; i < cars.Length - 1; i++)
+            {
+                for (int j = i + 1; j < cars.Length; j++)
+                {
+                    if (cars[i] == cars[j])
+                    {
+                        Console.WriteLine($"Car{i} = Car{j}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Car{i} and Car{j} are not equals");
+                    }
+                }
+            }
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i].ToString());
+            }
+        }
+    }
+}
