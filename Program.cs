9036376de47a2d5c3904c39ac959582e6341e010
Margo <97248807+Bahinska@users.noreply.github.com>
2022-05-15 20:43:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace hw5
+namespace hw6
 {
     class Program
     {
+        static int div_int(int first, int second)
+        {
+            if (second == 0) throw new DivideByZeroException();
+            return first/second;
+        }
+        static double div_double(double first, double second)
+        {
+            if (second == 0) throw new DivideByZeroException();
+            return first/second;
+        }
+            static int count= 0;
         static void Main(string[] args)
         {
-            List<IDeveloper> myList = new List<IDeveloper>();
-            myList.Add(new Programmer("C#"));
-            myList.Add(new Builder("Bolharka"));
-            myList.Add(new Builder("Sokyra"));
-            myList.Add(new Programmer("C++"));
-            myList.Add(new Programmer("Python"));
-            myList.Add(new Builder("Pylka"));
-            myList.Sort();
-
-            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            Dictionary<uint, string> myd=new Dictionary<uint, string>();
-            for (int i=0; i<7; i++)
-            {
-                Console.WriteLine("Enter ID and people name:");
-                uint id= (uint)Convert.ToInt32(Console.ReadLine());
-                string name = Console.ReadLine();
-                myd.Add(id, name);
-            }
-
-            Console.WriteLine("Enter ID to get people name:");
-            uint k = (uint)Convert.ToInt32(Console.ReadLine());
-            bool found = false;
-            foreach (KeyValuePair<uint, string> el in myd)
-            {
-                if (el.Key == k)
-                {
-                    Console.WriteLine($"Name: {el.Value}");
-                    found = true;
-                }
-            }
             try
             {
-                if (!found) throw new ArgumentException("Element does not exits");
+                Console.WriteLine("Enter two integers to divide");
+                int a = System.Convert.ToInt32(Console.ReadLine());
+                int b = System.Convert.ToInt32(Console.ReadLine());
+                int res = div_int(a, b);
+                Console.WriteLine($"It's equal {res}");
             }
-            catch (ArgumentException e)
+            catch (DivideByZeroException e)
             {
                 Console.WriteLine(e.Message);
             }
+            try
+            {
+                Console.WriteLine("Enter two integers with float coma to divide");
+                double x = System.Convert.ToDouble(Console.ReadLine());
+                double y = System.Convert.ToDouble(Console.ReadLine());
+
+                double res = div_double(x, y);
+                Console.WriteLine($"It's equal {res}");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            List<int> array = new List<int>();
+            Console.WriteLine("Please enter 10 numbers in ascending order");
+        proces:
+            bool was_ex = false;
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                     if(was_ex)i = count;
+                    int temp = System.Convert.ToInt32(Console.ReadLine());
+                    array.Add(temp);
+                    if (i != 0 && temp <= array[i - 1] ) throw new ArgumentException();
+                    count++;
+                    if (count == 10) break;
+                }
+            }
+            catch (ArgumentException e)
+            {
+                was_ex = true;
+                Console.WriteLine("Continue, but try not to break the rules");
+                goto proces;
+            }
+            
         }
     }
 }
