0285cae55372d983dce3b32431285c8097278234
Vlaad1955 <wylf1312@gmail.com>
2022-05-16 22:20:57 +0300

    RybalkoHomework

---
+++ RybalkoHomework5(1-2)/Builder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RybalkoHomework5
+{
+    internal class Builder : IDeveloper, IComparable
+    {
+        private string tool;
+        public string Tool { get { return tool; } set { tool = value; } }
+
+        public int CompareTo(object? o)
+        {
+            IDeveloper X = (IDeveloper)o;
+            if(X != null)
+            {
+                return tool.CompareTo(X.Tool);
+            }
+            else { return 0; }
+        }
+
+        public void Creat()
+        {
+            Console.WriteLine("Builders, creat");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine("Builders, destroy");
+        }
+    }
+}
---
+++ RybalkoHomework5(1-2)/RybalkoHomework5.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RybalkoHomework5
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Dictionary<uint, string> idpeopl = new Dictionary<uint, string>();
+            for(uint x=0; x<7; x++)
+            {
+                Console.Write($"ID {x+1} =");
+                uint id = Convert.ToUInt32(Console.ReadLine());
+                Console.Write($"Name {x+1} =");
+                idpeopl.Add(id, Console.ReadLine());
+            }
+            Console.Write("Enter the ID ");
+            uint i = Convert.ToUInt32(Console.ReadLine());
+            if (!idpeopl.ContainsKey(i))
+            {
+                Console.WriteLine("sorry, such id does not exist");
+            }
+                foreach (uint x in idpeopl.Keys)
+            {
+                if(i == x)
+                {
+                    Console.WriteLine(idpeopl[x]);
+                }
+            }
+        }
+    }
+}
