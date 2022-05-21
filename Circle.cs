e10fb80783c12b686f5f975d68c2a83b61073df8
Margo <97248807+Bahinska@users.noreply.github.com>
2022-05-21 12:30:45 +0300

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw8
+{
+    class Circle : Shape 
+    {
+        private double radius;
+
+        public double Radius
+            {
+            get { return radius; }
+            set { radius = value; }
+            }
+        public Circle(string n, double r) : base(n)
+        {
+            radius = r;
+        }
+        public override double Area() => Math.PI * radius * radius;
+        public override double Perimeter() => 2 * Math.PI * radius;
+    }
+}
