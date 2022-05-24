e7840b9508950ce8814bd44f3487aac9932f14cf
OksanaM <oks.sova2@gmail.com>
2022-05-25 00:43:31 +0300

    Add tsk9 + part of HW9

---
+++ MalaOks_HW9+tsk9/MalaOks_HW9/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalaOks_HW9
+{
+    internal class Square : Shape
+    {
+        public Square(double sd, string nm) : base(nm)
+        {
+            Side = sd;
+        }
+        public double Side { get; set; }
+        public override double Perimeter { get; set; }
+        public override double Area { get; set; }
+        public override void GetArea()
+        {
+            Area = Math.Pow(Side, 2);
+        }
+        public override void GetPerimeter()
+        {
+            Perimeter = Side * 4;
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Name: {Name}, area: {Area}, perimeter: {Perimeter}");
+        }
+    }
+}
---
+++ MalaOks_HW9+tsk9/Mala_HW9(2)/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+List<string> lst = new();
+string temp = String.Empty;
+using (StreamReader sr = new(@"D:\111.txt", System.Text.Encoding.Default))
+{
+    while ((temp = sr.ReadLine())!= null)
+    {
+        lst.Add(temp);
+    }
+}
+Console.WriteLine(lst.Count);
+Regex rgx = new(@"\W");
+//List<int> count = new();
+MatchCollection mtc; //= rgx.Matches();
+foreach (var item in lst)
+{
+    mtc = rgx.Matches(item);
+    if (mtc.Count != 0)
+    {
+        //count.Add(mtc.Count);
+        Console.WriteLine(mtc.Count);
+    }
+}
+
+//var qwr = lst.Where(n => n);
