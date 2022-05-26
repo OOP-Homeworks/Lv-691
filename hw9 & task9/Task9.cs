2ed742c5aa2a592dbe3d9b26253ae6c3eb40760f
lloran <maks57osipov@gmail.com>
2022-05-26 13:24:30 +0300

    Osypov_hw9_task9

---
+++ hw9 & task9/Task9.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task9
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write 10 numbers!");
+            List<int> numbers = new List<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter {i + 1} number: ");
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("----------------------");
+
+            IEnumerable<int> onlyNegative = numbers.Where(i => i < 0);
+            foreach(int i in onlyNegative)
+                Console.WriteLine("Negative: " + i);
+            Console.WriteLine("----------------------");
+
+            IEnumerable<int> onlyPositive = numbers.Where(i => i >= 0);
+            foreach (int i in onlyPositive)
+                Console.WriteLine("Positive: " + i);
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("Max: "+ numbers.Max());
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Average: " + numbers.Average());
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Sum: " + numbers.Sum());
+            Console.WriteLine("----------------------");
+            Console.WriteLine("First smallest than average: " + numbers.Where(i => i < numbers.Average()).Max());
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("Sorted list: ");
+            IEnumerable<int> sortedlist = numbers.OrderBy(i => i);
+            foreach(int i in sortedlist)
+                Console.WriteLine(i + "");
+            Console.ReadKey();
+        }
+    }
+}
