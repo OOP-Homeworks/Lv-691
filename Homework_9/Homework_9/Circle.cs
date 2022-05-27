1d081408524fb0b5cfa3c1cc039c2cad56597370
Svitlana <103998860+Chynchenko@users.noreply.github.com>
2022-05-27 18:11:52 +0300

    Add files via upload

---
+++ Homework_9/Homework_9/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Homework_9
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public double Radius { get { return radius; } }
+
+        public override double Name => throw new NotImplementedException();
+
+        public Circle(string name, double radius) : base(name)
+        {
+            this.radius = radius;
+        }
+
+        public override double Name()
+        {
+            return Math.PI * (radius * radius);
+        }
+
+        public override double Perimeter()
+        {
+            return Math.PI * 2 * radius;
+        }
+    }
+}
---
+++ Homework_9/Homework_9/Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Homework_9
+{
+    public class Square : Shape
+    {
+        private double side;
+
+        public double Side { get { return side; } }
+
+        public override double Name => throw new NotImplementedException();
+
+        public Square(string name, double side) : base(name)
+        {
+            this.side = side;
+        }
+
+        public override double Name() => (side * side);
+
+        public override double Perimeter()
+        {
+            return 4 * side;
+        }
+    }
+}
