4c9c577a24c902594f586aa30ec447e879452e57
Svitlana <103998860+Chynchenko@users.noreply.github.com>
2022-06-02 15:37:33 +0300

    Add files via upload

---
+++ Homework_11/Homework_11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework_11
+{
+    public delegate void MyDel(int f);
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Parent parent = new Parent();
+            Accountancy accountancy = new Accountancy();
+
+            MyDel f = parent.OnMarkChange;
+            MyDel s = accountancy.Stypendia;
+
+            student.MarkChange += f;
+            student.MarkChange += s;
+
+            student.AddMark(10);
+            student.AddMark(17);
+            student.AddMark(23);
+            student.AddMark(26);
+            student.AddMark(24);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_11/Homework_11/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using static Homework_11.Program;
+
+namespace Homework_11
+{
+    public class Student
+    {
+        private string name;
+
+        public string Name { get { return this.name = "Andriy"; } }
+        public List<int> marks = new List<int>();
+        public event MyDel MarkChange;
+
+        public void AddMark(int newMark)
+        {
+            marks.Add(10);
+            if (MarkChange != null)
+                MarkChange.Invoke(newMark);
+        }
+    }
+}
